cf0630c5395ce0a78317677ae204a8bb987bcbf3
Victoria <viktoriadmitrisena321@gmail.com>
2024-12-16 01:05:59 +0200

    Fixed code in line 8, updated the second test for account request

--- tests/test_http.py
+++ tests/test_http.py
@@ -5,9 +5,11 @@ import requests
 @pytest.mark.http
 def test_first_request():
     r = requests.get('https://api.github.com/zen')
-    print(r.text)
+    print(f"Response is {r.text}")
 
 @pytest.mark.http
 def test_second_request():
     r = requests.get('http://api.github.com/users/defunkt')
-    print(f"Response is {r.text}")
+    print(f"Response Body is {r.json()}")
+    print(f"Response Status code is {r.status_code}")
+    print(f"Response Headers are {r.headers}")
