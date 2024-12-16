4906367bc76df02afc2f9875a5923a3f6848d879
Victoria <viktoriadmitrisena321@gmail.com>
2024-12-16 17:44:24 +0200

    Updated HTTP tests to status_code, body, headers. Added a case-insensitive method

--- tests/test_http.py
+++ tests/test_http.py
@@ -9,7 +9,17 @@ def test_first_request():
 
 @pytest.mark.http
 def test_second_request():
-    r = requests.get('http://api.github.com/users/defunkt')
-    print(f"Response Body is {r.json()}")
-    print(f"Response Status code is {r.status_code}")
-    print(f"Response Headers are {r.headers}")
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200 
+    assert headers['Server'].lower() == 'github.com'.lower()
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+
+    assert r.status_code == 404 
