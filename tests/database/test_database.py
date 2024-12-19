b44af5c0b0376aead1505c22106bcc3b95cd2ba5
Victoria <viktoriadmitrisena321@gmail.com>
2024-12-19 17:42:51 +0200

    Added a list that checks if the data from the table is correct, using the list

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -14,3 +14,15 @@ def test_check_all_users():
     users = db.get_all_users()
 
     print(users)
+
+
+@pytest.mark.database
+def test_check_user_sergii():
+    db = Database()
+    user = db.get_user_address_by_name('Sergii')
+
+    assert user[0][0] == 'Maydan Nezalezhnosti 1'
+    assert user[0][1] == 'Kyiv'
+    assert user[0][2] == '3127'
+    assert user[0][3] == 'Ukraine'
+    
