6fd740542b32689b950137e10d3f7ff87c72a4b5
Victoria <viktoriadmitrisena321@gmail.com>
2024-12-20 21:41:19 +0200

    Added and modified method to insert or replace data in row with a unique identifier

--- modules/common/database.py
+++ modules/common/database.py
@@ -37,3 +37,10 @@ class Database():
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
+    
+
+    def insert_product(self, product_id, name, description, qnt):
+        query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) \
+            VALUES ({product_id}, '{name}', '{description}', {qnt})"
+        self.cursor.execute(query)
+        self.connection.commit()
