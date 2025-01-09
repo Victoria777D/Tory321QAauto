34eca3af24bf73b8be2d61d6150f123a673fc8f2
Victoria <viktoriadmitrisena321@gmail.com>
2025-01-09 05:47:00 +0200

    Added methods and tests for project

--- modules/common/database.py
+++ modules/common/database.py
@@ -62,12 +62,42 @@ class Database():
         record = self.cursor.fetchall()
         return record
 
-    #
+    # individual.
+    # додає значення неправильного типу у вказану таблицю і колонку 
+    def insert_invalid_type(self, table, column, value):
+        try:
+            query = f"INSERT INTO {table} ({column}) VALUES (?);"
+            self.cursor.execute(query, (value,))
+            self.connection.commit()
+            # щоб не спричиняло помилку TypeError
+            return None # повертає None, якщо помилки немає
+        except sqlite3.Error as e:
+            return str(e) # повертає повідомлення про помилку як рядок
+        
+
+    # додає дані до вказаної таблиці та перевіряє їх коректне додавання
+    def insert_and_validate_data(self, table, column, value):
+        try:
+            query = f"INSERT INTO {table} ({column}) VALUES (?);"
+            self.cursor.execute(query, (value,))
+            self.connection.commit()
+
+            # перевірка даних, що додали
+            select_query = f"SELECT {column} FROM {table} ORDER BY ROWID DESC LIMIT 1;"
+            self.cursor.execute(select_query)
+            inserted_value = self.cursor.fetchone()[0]
+            return inserted_value
+        except sqlite3.Error as e:
+            return str(e)
+        
+
     # вставляє нові значення якщо такий запис вже існує
     def insert_product(self, product_id, name, description, qnt):
         query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) VALUES ({product_id}, '{name}', '{description}', {qnt})"
         self.cursor.execute(query) # взаємодіє з базою даних виконуючи SQL-запит через об'єкт cursor
         self.connection.commit() # підтверджує зміни в базі даних після виконання запиту; без цього виклику зміни не будуть збережені
+
+
 
     # отримує всі дані про продукт
     def get_product_by_id(self, product_id):
@@ -75,6 +105,7 @@ class Database():
         self.cursor.execute(query) # виконує запит
         return self.cursor.fetchall() # повертає всі рядки, що відповідають запиту у вигляді списку
     
+
     # отримує кількість товару 
     def validate_quantity_is_positive(self, product_id):
         query = f"SELECT quantity FROM products WHERE id = {product_id}"
@@ -82,6 +113,7 @@ class Database():
         quantity = self.cursor.fetchone()[0] # отримує перший результат запиту,а також доступ до кількості товару  
         return quantity > 0 # істина,якщо більше нуля і похибка - якщо менше
     
+
     # вставляє дані
     def insert_data(self, table, columns, data):
         try:
@@ -106,6 +138,7 @@ class Database():
         self.cursor.execute(query, (record_id,))
         return self.cursor.fetchone()
         
+
     # формує запит щоб отримати всі продукти,де кількість товару більша або дорівнює min_gnt
     def get_all_products_with_minimum_quantity(self, min_qnt):
         query = f"SELECT * FROM products WHERE quantity >= {min_qnt}"
@@ -119,6 +152,7 @@ class Database():
         self.cursor.execute(query)
         return [row[0] for row in self.cursor.fetchall()]
     
+
     # вставляє кілька замовлень(параметр ? для запобігання SQl-ін'єкціям)
     def insert_bulk_orders(self, orders):
         query = "INSERT INTO orders (customer_id, product_id, order_date) VALUES (?, ?, ?)"
@@ -126,18 +160,21 @@ class Database():
         self.cursor.executemany(query, orders) # orders - список кортежів,що містить значення для одного замовлення
         self.connection.commit() # підтверджує зміни
 
+
     # отримує кількість всіх замовлень в orders
     def get_total_orders_count(self):
         query = "SELECT COUNT(*) FROM orders"
         self.cursor.execute(query) #виконує запит
         return self.cursor.fetchone()[0] # повертає перший результат запиту(кількість замовлень)
     
+
     # пошук всіх клієнтів чиє ім'я містить partial_name
     def get_customer_by_partial_name(self, partial_name):
-        # LIKE % для пошуку за частковим збігом 
+        # LIKE % для пошуку за частковим збігом(за частиною імені) 
         query = f"SELECT * FROM customers WHERE name LIKE '%{partial_name}%'"
         self.cursor.execute(query) # виконує запит
         return self.cursor.fetchall() # повертає рядки, які відповідають запиту 
+
 
     # додає новий запис у таблицю orders
     def insert_order(self, order_id, customer_id, product_id, order_date):
@@ -152,4 +189,4 @@ class Database():
             raise sqlite3.Error(f"Failed to insert order {order_id}. Error: {e}")    
              
 
-
+    
